5ab1fcee09fafccac1fa09a175f64173b55f7833
Scott Sum <scottsum@outlook.com>
2021-12-25 17:25:20 -0500

    added clock job

---
+++ job.py
@@ -0,0 +1,11 @@
+from wsgi import app
+from apscheduler.schedulers.background import BackgroundScheduler
+from nba_sentiment_analysis import job
+
+sched = BackgroundScheduler()
+
+@sched.scheduled_job('interval', hours='1')
+def update():
+	job(app)
+
+sched.start()
