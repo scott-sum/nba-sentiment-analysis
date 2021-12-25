a2433f3a2d4ceb45d88b5dc4d9bbe8b1507be619
Scott Sum <scottsum@outlook.com>
2021-12-24 20:35:27 -0500

    fix heroku postgres error uri postgresql

--- nba_sentiment_analysis/settings.py
+++ nba_sentiment_analysis/settings.py
@@ -1,6 +1,8 @@
 import os
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+	SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
