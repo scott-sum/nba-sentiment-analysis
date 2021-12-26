de682f0a254d1d78e5ff2c446625fc8647cc6a79
Scott Sum <scottsum@outlook.com>
2021-12-26 16:42:54 -0500

    prep for heroku scheduler

--- job.py
+++ job.py
@@ -1,12 +1,4 @@
 from wsgi import app
-from apscheduler.schedulers.background import BackgroundScheduler
 from nba_sentiment_analysis import job
 
-sched = BackgroundScheduler({'apscheduler.timezone': 'UTC'})
-
-@sched.scheduled_job('interval', hours=1)
-def update():
-	print("before job")
-	job(app)
-
-sched.start()
+job(app)
