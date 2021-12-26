b1c44972c699d916e1a1bbb6cb86609a42b191f3
Scott Sum <scottsum@outlook.com>
2021-12-25 21:09:44 -0500

    fix pytz deprecation add timezone to backgroundscheduler

--- job.py
+++ job.py
@@ -2,7 +2,7 @@ from wsgi import app
 from apscheduler.schedulers.background import BackgroundScheduler
 from nba_sentiment_analysis import job
 
-sched = BackgroundScheduler()
+sched = BackgroundScheduler({'apscheduler.timezone': 'UTC'})
 
 @sched.scheduled_job('interval', hours=1)
 def update():
